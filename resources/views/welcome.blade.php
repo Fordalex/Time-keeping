74c903a46dba8849b522406735ff85a41bd99362
Fordalex <alex96ford19@gmail.com>
2022-07-14 11:22:47 +0100

    clean up the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Invoicing</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('title', 'Home')
+@section('content')
 
-    <nav>
-        <ul>
-            <li>Home</li>
-        </ul>
-    </nav>
+    <h1>Home</h1>
 
-    <h1>Title</h1>
-
-</body>
-</html>
+@endsection
