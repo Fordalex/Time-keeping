d2b3213b902ac48965bec829c2423a45ee8e2b54
Fordalex <alex96ford19@gmail.com>
2022-07-15 19:51:28 +0100

    update seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 use DB;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +16,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // delete all
+        DB::table('shifts')->delete();
         DB::table('billed_shifts')->delete();
         DB::table('invoices')->delete();
+
+        // create shifts
+        DB::table('shifts')->insert([
+            'duration' => 60,
+            'hourly_rate' => 20.00,
+            'date' => Carbon::today()->subDays(1),
+        ]);
+        DB::table('shifts')->insert([
+            'duration' => 120,
+            'hourly_rate' => 20.00,
+            'date' => Carbon::today()->subDays(2),
+        ]);
+        DB::table('shifts')->insert([
+            'duration' => 90,
+            'hourly_rate' => 20.00,
+            'date' => Carbon::today()->subDays(3),
+        ]);
     }
 }
