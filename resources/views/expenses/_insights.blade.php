08370644953ef56751bc1e78db86e8bff2cecf39
Fordalex <alex96ford19@gmail.com>
2022-10-08 19:46:41 +0100

    remove days for expenses

--- resources/views/expenses/_insights.blade.php
+++ resources/views/expenses/_insights.blade.php
@@ -1,12 +1,6 @@
 <div class="py-4 flex justify-center">
     <div class="stats stats-horizontal lg:stats-horizontal shadow">
         <div class="stat">
-            <div class="stat-title">Days</div>
-            <div class="stat-value"></div>
-            <div class="stat-desc"></div>
-        </div>
-
-        <div class="stat">
             <div class="stat-title">Expenses</div>
             <div class="stat-value">{{ count($expenses) }}</div>
             <div class="stat-desc"></div>
